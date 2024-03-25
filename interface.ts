fb8a97bd12b9625cd4aaf8c37473be7cc7762db5
SuchAsYangPara <6633208521@student.chula.ac.th>
2024-03-25 20:51:59 +0700

    Card

--- interface.ts
+++ interface.ts
@@ -1,26 +1,28 @@
-interface HospitalItem {
-    _id: string,
+interface DentistItem {
+    id: string,
     name: string,
-    address: string,
-    district: string,
-    province: string,
-    postalcode: string,
-    tel: string,
-    picture: string,
-    __v: number,
-    id: string
+    picture:string,
+    experience: string;
+    expertise: string;
+    bookDate: Date[];
   }
   
-  interface HospitalJson {
+  interface DentistJson {
     success: boolean,
     count: number,
     pagination: Object,
-    data: HospitalItem[]
+    data: DentistItem[]
   }
 interface BookingItem{
-  name:string,
-  surname:string,
-  id:string,
-  hospital:string,
-  bookDate:string;
+  user: UserItem,
+  dentist: DentistItem,
+  bookingDate:Date,
+  createAt:Date
+}
+interface UserItem{
+  name: string,
+  tel: string,
+  email: string,
+  role: string,
+  password: string,
 }
