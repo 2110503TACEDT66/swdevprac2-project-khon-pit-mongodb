370f6be39e48dde327125d2e7e22dcbcb22c49bc
SuchAsYangPara <6633208521@student.chula.ac.th>
2024-03-25 11:36:57 +0700

    init

---
+++ src/providers/NextAuthProvider.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+
+export default function NextAuthProvider({children, session}
+    :{children:React.ReactNode,session:any}) : ReactNode{
+        return(
+            <SessionProvider session={session}>
+                {children}
+            </SessionProvider>
+        )
+    }
