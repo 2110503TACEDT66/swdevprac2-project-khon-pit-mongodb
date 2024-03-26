e0a4b9f6d5d048016994cbf49ad4cadefdb8b3bd
Icegoo141 <isoonthipthanasup@gmail.com>
2024-03-26 16:14:34 +0700

    fix interface

--- interface.ts
+++ interface.ts
@@ -1,31 +1,24 @@
 interface DentistItem {
-    id: string,
-    name: string,
-    picture:string,
-    experience: string;
-    expertise: string;
-    bookings: BookingItem[];
-  }
-  
-  interface DentistJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: DentistItem[]
-  }
-interface BookingItem{
-  _id:string,
-  user: Object,
-  dentist: Object,
-  bookingDate:Date,
-  symptom:string,
-  createAt:Date,
-  __v:number
-}
-interface UserItem {
+  id: string;
   name: string;
-  tel: string;
-  email: string;
-  role: string;
-  password: string;
+  picture: string;
+  experience: string;
+  expertise: string;
+  bookings: BookingItem[];
+}
+
+interface DentistJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: DentistItem[];
+}
+interface BookingItem {
+  _id: string;
+  user: { name: string };
+  dentist: DentistItem;
+  bookingDate: Date;
+  symptom: string;
+  createAt: Date;
+  __v: number;
 }
