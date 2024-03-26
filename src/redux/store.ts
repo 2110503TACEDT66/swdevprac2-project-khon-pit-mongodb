6444bf03872e52257d4b1e07488f4a917721fbfb
SuchAsYangPara <6633208521@student.chula.ac.th>
2024-03-26 16:17:57 +0700

    bookslice die

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import bookSlice from "./features/bookSlice";
 import dateSlice from "./features/dateSlice";
 import { useSelector,TypedUseSelectorHook } from "react-redux";
 import { persistReducer } from "redux-persist";
@@ -10,12 +9,10 @@ const persistConfig = {
     storage
 }
 
-const bookReducer = combineReducers({bookSlice})
-const bookPersistedReducer = persistReducer(persistConfig,bookReducer)
 const datePersistedReducer = persistReducer(persistConfig,combineReducers({dateSlice}))
 
 export const store = configureStore({
-    reducer:{bookPersistedReducer,datePersistedReducer}
+    reducer:{datePersistedReducer}
 })
 
 export type RootState = ReturnType<typeof store.getState>
